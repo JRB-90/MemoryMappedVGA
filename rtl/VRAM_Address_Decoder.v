a2d7581d086e9553163a0a262a614cc12f1e8267
Jacob Brown <Jake@insphereltd.com>
2021-02-05 16:44:41 +0000

    Added memory mapped VRAM for VGA pixel provider and simple memory init file for initialising the VRAM to white

--- rtl/VRAM_Address_Decoder.v
+++ rtl/VRAM_Address_Decoder.v
@@ -2,13 +2,13 @@ module VRAM_Address_Decoder(
 	input					clk,
 	input			[9:0]		px,
 	input			[9:0]		py,
-	output reg	[19:0]	address
+	output reg	[15:0]	address
 );
 
-`include	"../Libs/MyModules/VGA_Params.h"
+`include	"VGA_Params.h"
 
 always @(posedge clk) begin
-	address <= px + (pixelsH * py);
+	address <= (px + (pixelsH * py)) >> 2;
 end
 
 endmodule
